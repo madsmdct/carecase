24c9e75c269b005bb0e241fd9e35e7e094ff24e8
Mads Möller <mm@danishcare.dk>
2020-04-28 13:53:27 +0200

    sw almost ready
    still missing serial monitor values for adc read

--- SW/CareCase_Arduinov0.2/CareCase_Arduinov0.22.ino/CareCase_Arduinov0.22.ino.ino
+++ SW/CareCase_Arduinov0.2/CareCase_Arduinov0.22.ino/CareCase_Arduinov0.22.ino.ino
@@ -130,35 +130,6 @@ void pec(int x)
     case 12: lcd.print("BATT ON/OFF CTR"); break;
   }
 }
-/*
-void pec(int x)
-{
-  char txt[18];                               //aray consisting of 18 charecters called txt
-  sprintf(txt,"ERROR CODE %d",x);             //
-  lcd.clear();
-  lcd.print(txt);
-  lcd.setCursor(0,1);
-
-  switch(x)
-  {
-    case 0: Serial.print("OK"); break;
-    case 1: Serial.print("3v3 LDO"); break;
-    case 2: Serial.print("4v5 LDO GSM ONLY"); break;
-    case 3: Serial.print("DEFECTIVE FUSE"); break;
-    case 4: Serial.print("12V POWER MUX"); break;
-    case 5: Serial.print("12V POWER ON/OFF"); break;
-    case 6: Serial.print("5V POWER MUX"); break;
-    case 7: Serial.print("5V POWER BUCK"); break;
-    case 8: Serial.print("5V POWER MUX"); break;
-    case 9: Serial.print("5V BOOST SUPPLY"); break;
-    case 10: Serial.print("12V POWER MUX"); break;
-    case 11: Serial.print("12V BOOST CIRC."); break;
-    case 12: Serial.print("BATT ON/OFF CTR"); break;
-  }
-}
-*/
-
-
 
 void waitbutton()
 {
@@ -224,97 +195,50 @@ void loop() {
     delay(512);              // wait 1 sec
      
                                          
-       if(messure(9,647,703))                                             
-   //  if(messure(9,512,512))
-        {
+    if(messure(9,647,703))                                             
+    {
                                                         
-                         if(messure(6,607,743))
-                     //  if(messure(6,512,512))
-                          {
+        if(messure(6,607,743))
+        {
                                                        
-                                     if(!messure(11,573,777)) //Husk ! før messure
-                                     //  if(messure(11,512,512)) 
-                                      {
+            if(!messure(11,573,777)) //Husk ! før messure 
+            {
 
-                                      ErrorCode = 1;
-                                    /*pec(1);
-                                      Serial.print("FEJL-1");
-                                      Serial.print("\n");
-                                      waitbutton();
-           */
-                                      
-                                    }else if(!messure(10,550,766))
-                                //      }else if(messure(10,512,512)) 
-                                      {
-                                      ErrorCode = 2;  
-                            /*        pec(2);
-                                      Serial.print("FEJL-2");
-                                      Serial.print("\n");
-                                      waitbutton();
-                            */          } //ENDIF
+            ErrorCode = 1;
+            }else if(!messure(10,550,766))
+            {
+            ErrorCode = 2;  
+            } //ENDIF
 
-
-
-
-
-
-
-            
-                      }else if(messure(2,607,743))
-                    //     }else if(messure(2,512,512))
-                        {
-                        ErrorCode = 6;
-                /*      pec(6);
-                        Serial.print("Fejl-6");
-                        Serial.print("\n");
-                        waitbutton();
-                */
-              
-                        }else
-                        {
-                        ErrorCode = 7;
-                /*      pec(7);
-                        Serial.print("Fejl-7");
-                        Serial.print("\n");
-                        waitbutton();
-                */
-                        } //ENDIF
+      
+        }else if(messure(2,607,743))
+        {
+        ErrorCode = 6;
+        }else
+        {
+        ErrorCode = 7;
+        } //ENDIF
      
            
        
-      }else
+    }else
+    {
+        if(messure(8,647,703))
         {
-                      if(messure(8,647,703))
-                  //       if(messure(8,0,512))
+        ErrorCode = 3;  
+        }
+        else if(messure(1,647,703))
 
-                        {
-                        ErrorCode = 3;
-                   /*   pec(3);
-                        Serial.print("Fejl-3");
-                        Serial.print("\n");
-                        waitbutton(); 
-                   */     
-                        }
-                      else if(messure(1,647,703))
-                 //        else if(messure(1,512,512))
-                        {
-                        ErrorCode = 4;
-                   /*   pec(4);
-                        Serial.print("Fejl-4");
-                        Serial.print("\n");
-                     */ waitbutton();
-                         
-                        }
-                      else if(messure(0,647,703))
-                  //       else if(messure(0,512,512))
-                        {
-                        ErrorCode = 5;
-                  /*    pec(5);
-                        Serial.print("Fejl-5");
-                        Serial.print("\n");
-                        waitbutton(); 
-                  */    } //ENDIF
+        {
+        ErrorCode = 4;                         
+        }
+       
+        else if(messure(0,647,703))
+        {
+        ErrorCode = 5;
         } //ENDIF
+        
+    } //ENDIF
               delay(200);
 
        digitalWrite(6,LOW);
@@ -325,64 +249,42 @@ void loop() {
        digitalWrite(9,HIGH);
        delay(3000);   
 
-          if(messure(9,647,703))                                             
-     //          if(messure(9,512,512)) 
-              {
-                             if(!messure(6,607,743))
-                      //           if(messure(6,512,512))
-                                {
-                                           if(messure(5,607,743))
-                                      //         if(messure(5,512,512)) 
-                                              {
-                                              ErrorCode = 8;
-                                         /*   pec(8);
-                                              Serial.print("FEJL-8");
-                                              Serial.print("\n");
-                                              waitbutton();
-                                        */
-                                             }
-                                              else
-                                              {
-                                              ErrorCode = 9;
-                                         /*   pec(9);
-                                              Serial.print("FEJL-9");
-                                              Serial.print("\n");
-                                              waitbutton();
-                                         */   } //ENDIF
-                                 }//ENDIF
-              }else
-              {
+        if(messure(9,647,703))                                             
+        {
+            if(!messure(6,607,743))
+            {
+                  if(messure(5,607,743))
+                  {
+                  ErrorCode = 8;
+                  }
+                  else
+                  {
+                  ErrorCode = 9;
+                  } //ENDIF
+                  
+            }//ENDIF
+            
+        }else
+        {
                 
               if(messure(7,647,707))                                             
-                // if(messure(7,512,512))
+
                 {
                 ErrorCode = 10;  
-            /*  pec(10);
-                Serial.print("Fejl-10");
-                Serial.print("\n");
-                waitbutton(); 
-            */  }   
+                }   
          
            else if(messure(4,444,777))
-      //           else if(messure(4,512,512))
+
                 {
                 ErrorCode = 11;
-            /*  pec(11);
-                Serial.print("Fejl-11");
-                Serial.print("\n");
-                waitbutton(); 
-              */}
+                }
                  
                 else
                 {
                 ErrorCode = 12;
-            /*  pec(12);
-                Serial.print("Fejl-12");
-                Serial.print("\n");
-                waitbutton(); 
-            */  }//ENDIF
+                }//ENDIF
 
-              }//ENDIF 
+        }//ENDIF 
       delay(200);
     digitalWrite(9,LOW);
 
@@ -391,161 +293,7 @@ void loop() {
     
 
     waitbutton();
-/*          
-            
-             if((analogRead(A0) > 512)) 
-              {
-              lcd.clear();
-              lcd.print("Error Code 1");
-              lcd.setCursor(0,1);
-              lcd.print("3v3 LDO ");
-              Serial.print("FEJL-1");
-              Serial.print("\n");
-              waitbutton();
-            
-              }else(selectPort(2));
-           
-                  if((analogRead(A0) > 512))
-                    {
-                    lcd.clear();               
-                    lcd.print("Error Code 6");
-                    lcd.setCursor(0,1);
-                    lcd.print("5V POWER MUX");
-                    Serial.print("Fejl-6");
-                    Serial.print("\n");
-                    waitbutton();
-                
-                      }
-                      if((analogRead(A0) > 512))
-                        {
-                        lcd.clear();               
-                        lcd.print("Error Code 7");
-                        lcd.setCursor(0,1);
-                        lcd.print("5V POWER BUCK");
-                        Serial.print("Fejl-7");
-                        Serial.print("\n");
-                        waitbutton();
-                       }
-       
-   
-          
-/*
-
-  selectPort(9);
-    if((analogRead(A0) < 512))
-   {
-     lcd.clear();
-     lcd.print("FEJL-0   P9 TP16\n");
-     Serial.print("FEJL-0   P9 TP16");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-      
-  selectPort(6);
-    if((analogRead(A0) < 512))
-   {
-     lcd.clear();
-     lcd.print("FEJL-1   P8 TP8");
-     Serial.print("FEJL-1   P8 TP8");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-
-  selectPort(10);
-    if((analogRead(A0) < 512))
-   {
-     lcd.clear();
-     lcd.print("FEJL-2   P7 TP21");
-     Serial.print("FEJL-2   P7 TP21");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
- selectPort(3);
-    if((analogRead(A0) < 512))
-   {
-     lcd.clear();
-     lcd.print("FEJL-3   P6 TP27");
-     Serial.print("FEJL-3   P6 TP27");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-  selectPort(4);
-    if((analogRead(A0) < 512))
-   {
-     lcd.clear();
-     lcd.print("FEJL-4   P5 TP13");
-     Serial.print("FEJL-4   P5 TP13");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-
-  selectPort(5);
-    if((analogRead(A0) < 512))
-   {
-     lcd.clear();
-     lcd.print("FEJL-5   P4 TP19");
-     Serial.print("FEJL-5   P4 TP19");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-  
-  selectPort(7);
-    if((analogRead(A0) < 512))
-   {
-     lcd.clear();
-     lcd.print("FEJL-7   P2 TP14");
-     Serial.print("FEJL-7   P2 TP14");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-   selectPort(8);
-    if((analogRead(A0) > 1023))
-   {
-     lcd.clear();
-     lcd.print("FEJL-8   P23 TP14");
-     Serial.print("FEJL-8   P23 TP14");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-  selectPort(9);
-    if((analogRead(A0) > 1023))
-   {
-     lcd.clear();
-     lcd.print("FEJL-9   P22 TP23");
-     Serial.print("FEJL-9   P22 TP23");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-  selectPort(10);
-    if((analogRead(A0) > 1023))
-   {
-     lcd.clear();
-     lcd.print("FEJL-10   P21 TP17");
-     Serial.print("FEJL-10   P21 TP17");
-     Serial.print("\n");
-     waitbutton();
-     
-      }
-  selectPort(11);
-    if((analogRead(A0) > 1023))
-   {
-     lcd.clear();
-     lcd.print("FEJL-11   P20 TP20");
-     Serial.print("FEJL-11   P20 TP20");
-     Serial.print("\n");
-     waitbutton();
-     
-    }
-  */ 
+    
     
       lcd.setCursor(0,1);
       lcd.clear();
